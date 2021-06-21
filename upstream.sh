7e57b29c680ff20c25e83b8bb487158976c6cd3b
fernvenue <git@fernvenue.com>
2021-06-21 16:27:57 +0800

    Update upstream.sh

--- upstream.sh
+++ upstream.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget -O '/var/tmp/default.txt' https://cdn.jsdelivr.net/gh/fernvenue/adguard-home-upstream@master/v4.conf
-wget -O '/var/tmp/chinalist.txt' https://cdn.jsdelivr.net/gh/felixonmars/dnsmasq-china-list@master/accelerated-domains.china.conf
-wget -O '/var/tmp/applechina.txt' https://cdn.jsdelivr.net/gh/felixonmars/dnsmasq-china-list@master/apple.china.conf
+wget -O '/var/tmp/default.txt' https://cdn.jsdelivr.net/gh/fernvenue/adguard-home-upstream/v4.conf
+wget -O '/var/tmp/chinalist.txt' https://cdn.jsdelivr.net/gh/felixonmars/dnsmasq-china-list/accelerated-domains.china.conf
+wget -O '/var/tmp/applechina.txt' https://cdn.jsdelivr.net/gh/felixonmars/dnsmasq-china-list/apple.china.conf
 sed -i 's|server=|[|g' '/var/tmp/chinalist.txt'
 sed -i 's|114.114.114.114|]tls://223.5.5.5|g' '/var/tmp/chinalist.txt'
 sed -i 's|server=|[|g' '/var/tmp/applechina.txt'
