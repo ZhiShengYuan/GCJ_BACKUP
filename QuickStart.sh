78d5baa9494f59da6c2115051006a2a86d0bb4a0
fernvenue <git@fernvenue.com>
2021-07-15 17:19:18 +0800

    Update QuickStart.sh

--- QuickStart.sh
+++ QuickStart.sh
@@ -2,6 +2,7 @@
 sed -i 's|upstream_dns_file: ""|upstream_dns_file: /usr/local/AdGuardHome/upstream.txt|g' /usr/local/AdGuardHome/AdGuardHome.yaml
 curl -o '/usr/local/bin/upstream.sh' 'https://raw.githubusercontent.com/fernvenue/adguard-home-upstream/master/upstream.sh'
 chmod +x /usr/local/bin/upstream.sh
+/usr/local/bin/upstream.sh
 curl -o '/lib/systemd/system/upstream.service' 'https://raw.githubusercontent.com/fernvenue/adguard-home-upstream/master/upstream.service'
 curl -o '/lib/systemd/system/upstream.timer' 'https://raw.githubusercontent.com/fernvenue/adguard-home-upstream/master/upstream.timer'
 systemctl enable upstream.timer
