fade03ca160c5c79306287a5e9e808aee9790aa2
fernvenue <git@fernvenue.com>
2021-07-15 16:59:53 +0800

    Update QuickStart.sh

--- QuickStart.sh
+++ QuickStart.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sed -i 's|upstream_dns_file: ""|upstream_dns_file: /usr/local/AdGuardHome/upstream.txt|g' /usr/local/AdGuardHome/AdGuardHome.yaml
 curl -o '/usr/local/bin/upstream.sh' 'https://raw.githubusercontent.com/fernvenue/adguard-home-upstream/master/upstream.sh'
 chmod +x /usr/local/bin/upstream.sh
