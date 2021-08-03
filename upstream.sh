91d51a9dc6dcdc12083dc46c242c510408ec2eae
fernvenue <git@fernvenue.com>
2021-08-03 17:21:50 +0800

    Update upstream.sh

--- upstream.sh
+++ upstream.sh
@@ -6,6 +6,10 @@ sed -i 's|server=|[|g' '/var/tmp/chinalist.txt'
 sed -i 's|114.114.114.114|]tls://223.5.5.5|g' '/var/tmp/chinalist.txt'
 sed -i 's|server=|[|g' '/var/tmp/applechina.txt'
 sed -i 's|114.114.114.114|]tls://223.5.5.5|g' '/var/tmp/applechina.txt'
-cat '/var/tmp/default.txt' '/var/tmp/applechina.txt' '/var/tmp/chinalist.txt' > /usr/local/AdGuardHome/upstream.txt
-rm -rf '/var/tmp/default.txt' '/var/tmp/applechina.txt' '/var/tmp/chinalist.txt'
+# The following line is used to temporarily solve the issue that `upstream_dns_file` does not support Chinese domains.
+cat '/var/tmp/applechina.txt' '/var/tmp/chinalist.txt' | perl -CIOED -p -e 's/^.*\p{Script_Extensions=Han}.*$//g' > /var/tmp/upstream.txt
+sed -i '/^$/d' /var/tmp/upstream.txt
+# When the upstream solves this problem in the future, changes need to be made here.
+cat '/var/tmp/default.txt' '/var/tmp/upstream.txt' > /usr/local/AdGuardHome/upstream.txt
+rm -rf '/var/tmp/default.txt' '/var/tmp/applechina.txt' '/var/tmp/chinalist.txt' '/var/tmp/upstream.txt'
 systemctl restart AdGuardHome
